8917560a59077ccdb6abf0afcea94e0e31d6eef9
TXWSLYF <luyufeng0623@163.com>
2017-04-16 10:31:32 +0800

    addTextChanger

---
+++ TextChanger/handleDateTransform.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Alex on 2017/4/15.
+ */
+function formatTimeString(str) {                      //格式化时间字符串，转化为标准的时间格式 YYYY-MM-DD hh:mm:ss
+    var timePattern;
+    var year, month, day, hour, minute, second;
+    var timeContentArr = [];
+    var outStr = "";
+    if (str.indexOf("年") != (-1)) {                     //分中文日期字符串和英文日期字符串两种情况来考虑
+        timePattern = /(\d+)[^\d]+(\d+)[^\d]+(\d+)([^\d]+)(\d+):(\d+):(\d+)/;         //用来匹配中文日期字符串的正则表达式
+        timeContentArr = str.match(timePattern);
+        year = timeContentArr[1];
+        month = parseInt(timeContentArr[2]) < 10 ? "0" + timeContentArr[2] : timeContentArr[2];
+        day = parseInt(timeContentArr[3]) < 10 ? "0" + timeContentArr[3] : timeContentArr[3];
+        if (timeContentArr[4].indexOf("上午") != (-1)) {                          //判断是上午还是下午
+            if (parseInt(timeContentArr[5]) < 10) {                      //上午的话小时不变
+                hour = "0" + timeContentArr[5];
+            }
+            else {
+                hour = timeContentArr[5];
+            }
+        }
+        else {
+            hour = (parseInt(timeContentArr[5]) + 12).toString();      //下午的话小时加12
+        }
+        minute = timeContentArr[6];
+        second = timeContentArr[7];
+        outStr = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    }
+    else {
+        timePattern = /([a-zA-Z]+),[\u0020]+([a-zA-Z]+)[\u0020]+(\d+),[\u0020]+(\d+)[\u0020]+(\d+):(\d+):(\d+)[\u0020]+([a-zA-Z]+)/;      //用来匹配英文日期字符串的正则表达式
+        timeContentArr = str.match(timePattern);
+        year = timeContentArr[4];
+        month = getMonthNumber(timeContentArr[2]);
+        day = parseInt(timeContentArr[3]) < 10 ? "0" + timeContentArr[3] : timeContentArr[3];
+        if (timeContentArr[8].indexOf("AM") != (-1)) {                   //判断是早上还是晚上
+            if (parseInt(timeContentArr[5]) < 10) {                      //上午的话小时不变
+                hour = "0" + timeContentArr[5];
+            }
+            else {
+                hour = timeContentArr[5];
+            }
+        }
+        else {
+            hour = (parseInt(timeContentArr[5]) + 12).toString();
+        }
+        minute = timeContentArr[6];
+        second = timeContentArr[7];
+        outStr = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    }
+    return outStr;
+}
+
+function getMonthNumber(str) {                      //将月份的字符串转化为数字
+    var outPut;
+    var monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if ((monthArr.indexOf(str) + 1) < 10) {
+        outPut = "0" + (monthArr.indexOf(str) + 1).toString();
+    }
+    else {
+        outPut = (monthArr.indexOf(str) + 1).toString();
+    }
+    return outPut;
+}
+
+function getTimeString(str) {          //对获取的时间信息做进一步的处理
+    var outStr = "";
+    if (str.indexOf("| 添加于 ") != (-1)) {
+        outStr = str.replace("| 添加于 ", "");
+    }
+    else {
+        outStr = str.replace("| Added on ", "");
+    }
+    return outStr;
+}
---
+++ TextChanger/main.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Alex on 2017/4/14.
+ */
+window.onload = function () {
+    var readFile = new FileReader();
+    var fileInput = document.getElementById("getFile");
+    var pattern = /(.+)\((.+)\)[^\d]+(\d+(?:-\d+)?).+(\|.+:\d+)(\s+[AM|PM]+)?(.+)?/;          //用来匹配注释的正则表达式
+    var titleOl = document.getElementById("titleList");
+    var titleList = []; //用来保存标题的数组
+    fileInput.onchange = function () {
+        var i, j, k;
+        for (i = 0; i < fileInput.files.length; i++) {
+            var file = fileInput.files[i];
+            readFile.readAsText(file);
+            readFile.onload = function () {
+                var result = readFile.result.replace(/[\f\n\r\t\u000B\u00A0\u2028\u2029\u21B5]+/g, "");//先将字符串中除了空格字符之外的所有空白字符删除(考虑到英文的问题，所以不可以删除空格符号)
+                var annotationArr = result.split(/={7,}/);         //获取数组形式的注释(=大于等于7个的时候才分割，避免注释其它部分出现少量等号而导致的错误分割的情况发生)
+                deleteEmptyArr(annotationArr);                  //将数组中的空数组删除
+                for (j = 0; j < annotationArr.length; j++) {
+                    var informations = annotationArr[j].match(pattern);
+                    console.log(informations);
+                    if (informations[6] == undefined) continue;                   //如果注释的内容为空，则直接跳过该条注释
+                    var annotationContainer = document.createElement("div");       //创建一个annotationContainer来容纳一条注释
+                    annotationContainer.className = "annotationContainer";
+                    for (k = 1; k < informations.length; k++) {
+                        switch (k) {
+                            case 1:
+                                var title = document.createElement("h2");
+                                title.innerHTML = "title：" + "《" + informations[1].trim() + "》";
+                                if (titleList.indexOf("《" + informations[1].trim() + "》") == (-1)) {            //如果新的标题不存在于标题数组之中
+                                    titleList.push("《" + informations[1].trim() + "》");                    //将新的标题添加到标题数组之中
+                                    var li = document.createElement("li");                           //创建一个新的包含标题的li标签并且添加到列表当中
+                                    li.innerHTML = "《" + informations[1].trim() + "》";
+                                    titleOl.appendChild(li);
+                                }
+                                annotationContainer.appendChild(title);
+                                break;
+                            case 2:
+                                var author = document.createElement("div");
+                                author.innerHTML = "author：" + informations[2];
+                                annotationContainer.appendChild(author);
+                                break;
+                            case 3:
+                                var position = document.createElement("div");
+                                position.innerHTML = "position：" + informations[3];
+                                annotationContainer.appendChild(position);
+                                break;
+                            case 4:
+                                var time = document.createElement("div");
+                                time.innerHTML = "time：" + getTimeString(informations[4]);            //对之前提取的时间信息进行进一步分析
+                                annotationContainer.appendChild(time);
+                                break;
+                            case 5:                                                                 //对第五个捕获型分组进行判断
+                                if (informations[5] != undefined) {                                 //如果其不等于空，则将其添加到时间信息后面
+                                    time.innerHTML = "time：" + getTimeString(informations[4]) + informations[5];
+                                    time.innerHTML = time.innerHTML + "——————"+formatTimeString(getTimeString(informations[4]) + informations[5]);
+                                    break;
+                                }
+                                else {
+                                    time.innerHTML = time.innerHTML +"——————"+ formatTimeString(getTimeString(informations[4]));
+                                    break;
+                                }
+                            default:
+                                var content = document.createElement("div");
+                                content.innerHTML = "content：" + informations[6];
+                                annotationContainer.appendChild(content);
+                                break;
+                        }
+                    }
+                    document.body.appendChild(annotationContainer);
+                }
+            }
+        }
+    }
+};
+
+function deleteEmptyArr(arr) {              //删除数组中的空数组
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == "") {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+
+
+
