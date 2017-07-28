6056a3aa06c07aff358f9f8122a0ba127d1b4059
TXWSLYF <luyufeng0623@163.com>
2017-07-28 14:44:56 +0800

    添加了拖拽上传

--- demos/HTML5拖拽上传/main.js
+++ demos/HTML5拖拽上传/main.js
@@ -3,6 +3,7 @@
  */
 
 var singleUpload = document.getElementById('single-file-upload');
+var manualFileChooser = document.getElementsByClassName('manual-file-chooser')[0];
 
 //为singleUpload添加ondragover事件，阻止其默认触发事件。
 singleUpload.addEventListener('dragover', function (e) {
@@ -11,7 +12,23 @@ singleUpload.addEventListener('dragover', function (e) {
 
 singleUpload.addEventListener('drop', function (e) {
     e.preventDefault();
+    var i = 0;
 
     //e.dataTransfer.files包含拖拽释放的文件对象
-    console.log(e.dataTransfer.files);
+    var fileList = e.dataTransfer.files;
+
+    for (i; i < fileList.length; i++) {
+        console.log(fileList[i].name)
+    }
+});
+
+manualFileChooser.addEventListener('change', function (e) {
+    var i = 0;
+
+    //files属性保存着上传文件的FileList对象
+    var fileList = manualFileChooser.files;
+
+    for (i; i < fileList.length; i++) {
+        console.log(fileList[i].name)
+    }
 });
