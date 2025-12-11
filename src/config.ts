f518c80e48899bc0cc7725c54d5a8b7e54ca52fe
alex <1060319328@qq.com>
2025-12-11 11:53:12 +0900

    feat: update posts per page from 3 to 5

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const SITE: Site = {
   // TODO:
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
